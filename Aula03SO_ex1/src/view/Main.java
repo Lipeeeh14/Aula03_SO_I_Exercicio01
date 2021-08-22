b250675addc4b8341fd50d97469f04e296255d38
Luiz Felipe <lipeeeh14@gmail.com>
2021-08-21 21:01:53 -0300

    Alteracoes na main

--- Aula03SO_ex1/src/view/Main.java
+++ Aula03SO_ex1/src/view/Main.java
@@ -9,25 +9,28 @@ public class Main {
 	public static void main(String[] args) {
 		RedesController rdsController = new RedesController();
 		int opcao;
-		do {
-			opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite o que deseja fazer ?\n"
-					+ "1 - Verificar o IP\n"
-					+ "2 - Verificar a média do ping\n"
-					+ "3 - Finalizar"));
-			
-			switch(opcao) {
-				case 1:
-					rdsController.ip();
-					break;
-				case 2:
-					rdsController.ping();
-					break;
-				case 3:
-					JOptionPane.showMessageDialog(null, "Até a próxima!!!");
-					break;
-				default:
-					JOptionPane.showMessageDialog(null, "Opção inválida, digite novamente!");
-			}
-		} while(opcao != 3);
+			try {
+				do {
+					opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite o que deseja fazer ?\n"
+							+ "1 - Verificar o IP\n"
+							+ "2 - Verificar a média do ping\n"
+							+ "3 - Finalizar"));
+					switch(opcao) {
+						case 1:
+							rdsController.ip();
+							break;
+						case 2:
+							rdsController.ping();
+							break;
+						case 3:
+							JOptionPane.showMessageDialog(null, "Até a próxima!!!");
+							break;
+						default:
+							JOptionPane.showMessageDialog(null, "Opção inválida, digite novamente!");
+					}
+				} while(opcao != 3);
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Por favor digite um número");
+			}	
 	}
 }
